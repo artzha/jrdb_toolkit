98c123b656363d945f2762ff0b582da564724fb6
artzha <arthurz@utexas.edu>
2023-08-21 15:56:59 -0500

    Parallelized object evaluation

--- detection_eval/run_obj_eval.bash
+++ detection_eval/run_obj_eval.bash
@@ -7,7 +7,7 @@ fi
 
 input_directory="$1"
 epoch="$2"
-outdir="$3"
+output_directory="$3"
 
 # Check if the input directory exists
 if [ ! -d "$input_directory" ]; then
@@ -23,38 +23,45 @@ if ! [[ "$epoch" =~ ^[0-9]+$ ]]; then
 fi
 
 # Check if the output directory exists, and create it if not
-if [ ! -d "$outdir" ]; then
-    mkdir -p "$outdir"
-    echo "Created output directory: $outdir"
+if [ ! -d "$output_directory" ]; then
+    mkdir -p "$output_directory"
+    echo "Created output directory: $output_directory"
 fi
 
-if [ "$epoch" -eq 0 ]; then
-    echo "Evaluating all epochs"
-    # Iterate through all epochs
-    for epoch in $(seq 1 30); do
-        gt_directory="$input_directory/epoch_$epoch/val/final_result/data/jrdb_gt"
-        dt_directory="$input_directory/epoch_$epoch/val/final_result/data/jrdb_preds"
-        outfile="$outdir/outfile$epoch.txt"
+# Function to evaluate an epoch
+evaluate_epoch() {
+    local epoch=$1
 
-        echo "Evaluating epoch $epoch"
-        echo "Ground truth directory: $gt_directory"
-        echo "Detection directory: $dt_directory"
-        echo "Output file: $outfile"
-        
-        # Call the evaluate_object script for each epoch
-        ./evaluate_object "$input_directory" "$epoch" 1 $outfile 0
-    done
-else
-    gt_directory="$input_directory/epoch_$epoch/val/final_result/data/jrdb_gt"
-    dt_directory="$input_directory/epoch_$epoch/val/final_result/data/jrdb_preds"
-    outfile="outfile$epoch.txt"
+    local gt_directory="$input_directory/epoch_$epoch/val/final_result/data/jrdb_gt"
+    local dt_directory="$input_directory/epoch_$epoch/val/final_result/data/jrdb_preds"
+    local outfile="$output_directory/outfile$epoch.txt"
 
     echo "Evaluating epoch $epoch"
     echo "Ground truth directory: $gt_directory"
     echo "Detection directory: $dt_directory"
     echo "Output file: $outfile"
 
-    ./evaluate_object $gt_directory $dt_directory 1 $outfile 0
+    ./evaluate_object "$input_directory" "$epoch" 1 $outfile 0
+}
+
+
+if [ "$epoch" -eq 0 ]; then
+    echo "Evaluating all epochs"
+    # Maximum number of concurrent threads
+    max_threads=4
+
+    # Iterate through all epochs
+    for epoch in $(seq 1 30); do
+        # Start a new thread to evaluate the current epoch
+        evaluate_epoch "$epoch" &
+
+        # Limit the number of concurrent threads
+        if [ $(jobs -r -p | wc -l) -ge "$max_threads" ]; then
+            wait -n
+        fi
+    done
+else
+    evaluate_epoch "$epoch"
 fi
 
 echo "Completed Evaluation."
