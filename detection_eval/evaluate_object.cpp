3a064f4dabd2317bbed3ff916f9d846afd44ffb4
Arthur K Zhang <arthurz@cs.utexas.edu>
2023-09-04 12:21:47 -0500

    Added support for tp, fp, fn result saving

--- detection_eval/evaluate_object.cpp
+++ detection_eval/evaluate_object.cpp
@@ -9,6 +9,7 @@
 #include <assert.h>
 #include <sys/types.h>
 #include <stdexcept>
+#include <filesystem>
 
 #include <dirent.h>
 
@@ -446,6 +447,36 @@ void cleanData(CLASSES current_class, const vector<tGroundtruth> &gt, const vect
   }
 }
 
+void write_stat_result(string outfilepre, const vector<vector<vector<int32_t> > > &tp_indices, const vector<vector<vector<int32_t> > > &fp_indices, const vector<vector<vector<int32_t> > > &fn_indices, int32_t thres_idx, int32_t frame) {
+  /*
+  num frames
+    num thresholds
+      detection indices (variable)
+  */
+  ofstream outfile;
+  string outfilename = outfilepre + "tp.txt";
+  outfile.open(outfilename);
+  for (auto tp_det_index : tp_indices[frame][thres_idx]) {
+      outfile << tp_det_index << '\n';
+  }
+  outfile.close();
+
+  outfilename = outfilepre + "fp.txt";
+  outfile.open(outfilename);
+  for (auto fp_det_index : fp_indices[frame][thres_idx]) {
+    outfile << fp_det_index << '\n';
+  }
+  outfile.close();
+
+  outfilename = outfilepre + "fn.txt";
+  outfile.open(outfilename);
+  for (auto fn_stat : fn_indices[frame][thres_idx]) {
+    outfile << fn_stat << '\n';
+  }
+  outfile.close();
+}
+
+// default version
 tPrData computeStatistics(CLASSES current_class, const vector<tGroundtruth> &gt,
         const vector<tDetection> &det, const vector<tGroundtruth> &dc,
         const vector<int32_t> &ignored_gt, const vector<int32_t>  &ignored_det,
@@ -602,6 +633,172 @@ tPrData computeStatistics(CLASSES current_class, const vector<tGroundtruth> &gt,
         stat.similarity = -1;
     }
   }
+
+  return stat;
+}
+
+// custom version
+tPrData computeStatistics(CLASSES current_class, const vector<tGroundtruth> &gt,
+        const vector<tDetection> &det, const vector<tGroundtruth> &dc,
+        const vector<int32_t> &ignored_gt, const vector<int32_t>  &ignored_det,
+        bool compute_fp, double (*boxoverlap)(tDetection, tGroundtruth, int32_t),
+        METRIC metric, 
+        vector<int32_t> &tp_indices, vector<int32_t> &fp_indices, vector<int32_t> &fn_indices,
+        bool compute_aos=false, double thresh=0, bool debug=false){
+  tPrData stat = tPrData();
+  const double NO_DETECTION = -10000000;
+  vector<double> delta;            // holds angular difference for TPs (needed for AOS evaluation)
+  vector<bool> assigned_detection; // holds wether a detection was assigned to a valid or ignored ground truth
+  assigned_detection.assign(det.size(), false);
+  vector<bool> ignored_threshold;
+  ignored_threshold.assign(det.size(), false); // holds detections with a threshold lower than thresh if FP are computed
+
+  // detections with a low score are ignored for computing precision (needs FP)
+  if(compute_fp)
+    for(int32_t i=0; i<det.size(); i++)
+      if(det[i].thresh<thresh)
+        ignored_threshold[i] = true;
+
+  // evaluate all ground truth boxes
+  for(int32_t i=0; i<gt.size(); i++){
+
+    // this ground truth is not of the current or a neighboring class and therefore ignored
+    if(ignored_gt[i]==-1)
+      continue;
+
+    /*=======================================================================
+    find candidates (overlap with ground truth > 0.5) (logical len(det))
+    =======================================================================*/
+    int32_t det_idx          = -1;
+    double valid_detection = NO_DETECTION;
+    double max_overlap     = 0;
+
+    // search for a possible detection
+    bool assigned_ignored_det = false;
+    for(int32_t j=0; j<det.size(); j++){
+
+      // detections not of the current class, already assigned or with a low threshold are ignored
+      if(ignored_det[j]==-1)
+        continue;
+      if(assigned_detection[j])
+        continue;
+      if(ignored_threshold[j])
+        continue;
+      // find the maximum score for the candidates and get idx of respective detection
+      double overlap = boxoverlap(det[j], gt[i], -1);
+
+      // for computing recall thresholds, the candidate with highest score is considered
+      if(!compute_fp && overlap>MIN_OVERLAP[metric][current_class] && det[j].thresh>valid_detection){
+        det_idx         = j;
+        valid_detection = det[j].thresh;
+      }
+
+      // for computing pr curve values, the candidate with the greatest overlap is considered
+      // if the greatest overlap is an ignored detection, the overlapping detection is used
+      else if(compute_fp && overlap>MIN_OVERLAP[metric][current_class] && (overlap>max_overlap || assigned_ignored_det) && ignored_det[j]==0){
+        max_overlap     = overlap;
+        det_idx         = j;
+        valid_detection = 1;
+        assigned_ignored_det = false;
+      }
+      else if(compute_fp && overlap>MIN_OVERLAP[metric][current_class] && valid_detection==NO_DETECTION && ignored_det[j]==1){
+        det_idx              = j;
+        valid_detection      = 1;
+        assigned_ignored_det = true;
+      }
+    }
+
+    /*=======================================================================
+    compute TP, FP and FN
+    =======================================================================*/
+
+    // nothing was assigned to this valid ground truth
+    if(valid_detection==NO_DETECTION && ignored_gt[i]==0) {
+      stat.fn++;
+      fn_indices.push_back(i);
+    }
+
+    // only evaluate valid ground truth <=> detection assignments
+    else if(valid_detection!=NO_DETECTION && (ignored_gt[i]==1 || ignored_det[det_idx]==1))
+      assigned_detection[det_idx] = true;
+
+    // found a valid true positive
+    else if(valid_detection!=NO_DETECTION){
+
+      // write highest score to threshold vector
+      stat.tp++;
+      stat.v.push_back(det[det_idx].thresh);
+      tp_indices.push_back(i);
+
+      // compute angular difference of detection and ground truth if valid detection orientation was provided
+      if(compute_aos)
+        delta.push_back(gt[i].box.alpha - det[det_idx].box.alpha);
+
+      // clean up
+      assigned_detection[det_idx] = true;
+    }
+  }
+
+  // if FP are requested, consider stuff area
+  if(compute_fp){
+
+    // count fp
+    for(int32_t i=0; i<det.size(); i++){
+
+      // count false positives if required (height smaller than required is ignored (ignored_det==1)
+      if(!(assigned_detection[i] || ignored_det[i]==-1 || ignored_det[i]==1 || ignored_threshold[i]))
+        stat.fp++;
+        fp_indices.push_back(i);
+    }
+
+    // do not consider detections overlapping with stuff area
+    int32_t nstuff = 0;
+    for(int32_t i=0; i<dc.size(); i++){
+      for(int32_t j=0; j<det.size(); j++){
+
+        // detections not of the current class, already assigned, with a low threshold or a low minimum height are ignored
+        if(assigned_detection[j])
+          continue;
+        if(ignored_det[j]==-1 || ignored_det[j]==1)
+          continue;
+        if(ignored_threshold[j])
+          continue;
+
+        // compute overlap and assign to stuff area, if overlap exceeds class specific value
+        double overlap = boxoverlap(det[j], dc[i], 0);
+        if(overlap>MIN_OVERLAP[metric][current_class]){
+          assigned_detection[j] = true;
+          nstuff++;
+        }
+      }
+    }
+
+    // FP = no. of all not to ground truth assigned detections - detections assigned to stuff areas
+    stat.fp -= nstuff;
+
+    // if all orientation values are valid, the AOS is computed
+    if(compute_aos){
+      vector<double> tmp;
+
+      // FP have a similarity of 0, for all TP compute AOS
+      tmp.assign(stat.fp, 0);
+      for(int32_t i=0; i<delta.size(); i++)
+        tmp.push_back((1.0+cos(delta[i]))/2.0);
+
+      // be sure, that all orientation deltas are computed
+      assert(tmp.size()==stat.fp+stat.tp);
+      assert(delta.size()==stat.tp);
+
+      // get the mean orientation similarity for this image
+      if(stat.tp>0 || stat.fp>0)
+        stat.similarity = accumulate(tmp.begin(), tmp.end(), 0.0);
+
+      // there was neither a FP nor a TP, so the similarity is ignored in the evaluation
+      else
+        stat.similarity = -1;
+    }
+  }
+
   return stat;
 }
 
@@ -609,6 +806,7 @@ tPrData computeStatistics(CLASSES current_class, const vector<tGroundtruth> &gt,
 EVALUATE CLASS-WISE
 =======================================================================*/
 
+// default version
 bool eval_class(CLASSES current_class,
         const vector< vector<tGroundtruth> > &groundtruth,
         const vector< vector<tDetection> > &detections, bool compute_aos,
@@ -685,6 +883,7 @@ bool eval_class(CLASSES current_class,
   return true;
 }
 
+// custom version
 bool eval_class(CLASSES current_class,
         const vector< vector<tGroundtruth> > &groundtruth,
         const vector< vector<tDetection> > &detections, bool compute_aos,
@@ -700,6 +899,10 @@ bool eval_class(CLASSES current_class,
   vector< vector<int32_t> > ignored_gt, ignored_det;  // index of ignored gt detection for current class
   vector< vector<tGroundtruth> > dontcare;            // index of dontcare areas, included in ground truth
 
+  vector<int32_t> tmp_tp_indices;
+  vector<int32_t> tmp_fp_indices;
+  vector<int32_t> tmp_fn_indices;
+
   // for all test images do
   for (int32_t i=0; i<groundtruth.size(); i++){
 
@@ -715,7 +918,12 @@ bool eval_class(CLASSES current_class,
 
     // compute statistics to get recall values
     tPrData pr_tmp = tPrData();
-    pr_tmp = computeStatistics(current_class, groundtruth[i], detections[i], dc, i_gt, i_det, false, boxoverlap, metric);
+    pr_tmp = computeStatistics(current_class, groundtruth[i], detections[i], dc, i_gt, i_det, false, boxoverlap, metric, tmp_tp_indices, tmp_fp_indices, tmp_fn_indices);
+    // pr_tmp = computeStatistics(current_class, groundtruth[i], detections[i], dc, i_gt, i_det, true, boxoverlap, metric); // compute recall for this
+    //Empty temp stat vectors
+    tmp_tp_indices.clear();
+    tmp_fp_indices.clear();
+    tmp_fn_indices.clear();
 
     // add detection scores to vector over all images
     for(int32_t j=0; j<pr_tmp.v.size(); j++)
@@ -724,6 +932,16 @@ bool eval_class(CLASSES current_class,
   // cout << "ignored gt " << ignored_gt.size() << " ignored det " << ignored_det.size() << " dontcare " << dontcare.size() << endl;
   // get scores that must be evaluated for recall discretization
   thresholds = getThresholds(v, n_gt);
+  // cout << "Thesholds: ";
+  // for (auto threshold : thresholds) {
+  //   cout << threshold << " ";
+  // }
+  // cout << "\n";
+  const size_t N_FRAMES = groundtruth.size();
+  const size_t N_THRESHOLDS = thresholds.size();
+  vector<vector<vector<int32_t> > > tp_indices(N_FRAMES, vector<vector<int32_t> >(N_THRESHOLDS, vector<int32_t>()) );
+  vector<vector<vector<int32_t> > > fp_indices(N_FRAMES, vector<vector<int32_t> >(N_THRESHOLDS, vector<int32_t>()) );
+  vector<vector<vector<int32_t> > > fn_indices(N_FRAMES, vector<vector<int32_t> >(N_THRESHOLDS, vector<int32_t>()) );
 
   // compute TP,FP,FN for relevant scores
   vector<tPrData> pr;
@@ -735,36 +953,55 @@ bool eval_class(CLASSES current_class,
       tPrData tmp = tPrData();
       tmp = computeStatistics(current_class, groundtruth[i], detections[i], dontcare[i],
                               ignored_gt[i], ignored_det[i], true, boxoverlap, metric,
+                              tp_indices[i][t], fp_indices[i][t], fn_indices[i][t],
                               compute_aos, thresholds[t], t==38);
-
       // add no. of TP, FP, FN, AOS for current frame to total evaluation for current threshold
       pr[t].tp += tmp.tp;
       pr[t].fp += tmp.fp;
       pr[t].fn += tmp.fn;
+      // if (tp_indices[i][t].size()>0) 
+      //   cout << "tp indices " << t << " parent func size " << tp_indices[i][t].size() << '\n';
       if(tmp.similarity!=-1)
         pr[t].similarity += tmp.similarity;
     }
   }
 
   // compute recall, precision and AOS
-  precision.assign(N_SAMPLE_PTS, 0);
-  recall.assign(N_SAMPLE_PTS, 0);
+  precision.assign(N_THRESHOLDS, 0); // potential bug, if num thresholds < N_SAMPLE_PTS
+  recall.assign(N_THRESHOLDS, 0);
+  // precision.assign(N_SAMPLE_PTS, 0); // potential bug, if num thresholds < N_SAMPLE_PTS
+  // recall.assign(N_SAMPLE_PTS, 0);
   double r=0;
   for (int32_t i=0; i<thresholds.size(); i++){
     r = pr[i].tp/(double)(pr[i].tp + pr[i].fn);
     precision[i] = pr[i].tp/(double)(pr[i].tp + pr[i].fp);
-    recall[i] = pr[i].tp/(double)(pr[i].tp + pr[i].fn);
+    recall[i] = r;
   }
 
-  // filter precision and AOS using max_{i..end}(precision)
-  for (int32_t i=0; i<thresholds.size(); i++){
-    precision[i] = *max_element(precision.begin()+i, precision.end());
-    recall[i] = *max_element(recall.begin()+i, recall.end());
+  // Save predictions from threshold with highest precision
+  const size_t VIS_THRES_INDEX = size_t(N_THRESHOLDS/2);
+
+  for (int32_t frame = 0; frame<groundtruth.size(); ++frame) {
+    // cout << "Saving evaluations with for frame " << to_string(frame) << " with max threshold " << to_string(max_thres_index) << '\n';
+
+    string outfilepre = "/robodata/arthurz/Benchmarks/jrdb_toolkit/detection_eval/eval_pr_out/" + to_string(frame) + "/";
+    filesystem::create_directory(outfilepre);
+
+    write_stat_result(outfilepre, tp_indices, fp_indices, fn_indices, VIS_THRES_INDEX, frame);
   }
+  cout << "Done writing tp, fp, fn results to files\n";
+
+  // Don't apply precision filter because it amplifies precision, affecting f1 score
+  // filter precision and AOS using max_{i..end}(precision)
+  // for (int32_t i=0; i<N_THRESHOLDS; i++){
+  //   precision[i] = *max_element(precision.begin()+i, precision.end());
+  //   // recall[i] = *max_element(recall.begin()+i, recall.end());
+  // }
 
   return true;
 }
 
+// default version
 void write_result(ofstream& outfile, string exp_name, vector<double> &precisions) {
   double ap = accumulate(precisions.begin() + 1, precisions.end(), 0.0) / (N_SAMPLE_PTS - 1);
   outfile << exp_name << "," << ap ;
@@ -774,9 +1011,11 @@ void write_result(ofstream& outfile, string exp_name, vector<double> &precisions
   outfile << endl;
 }
 
+// custom version
 void write_result(ofstream& outfile, string exp_name, vector<double> &precisions, vector<double> &recalls) {
-  double ap = accumulate(precisions.begin() + 1, precisions.end(), 0.0) / (N_SAMPLE_PTS - 1);
-  double ar = accumulate(recalls.begin() + 1, recalls.end(), 0.0) / (N_SAMPLE_PTS - 1);
+  // fixes tp average computation
+  double ap = accumulate(precisions.begin(), precisions.end(), 0.0) / (precisions.size());
+  double ar = accumulate(recalls.begin(), recalls.end(), 0.0) / (recalls.size());
   double af1 = (double)(2 * ap * ar) / (ap + ar);
   outfile << exp_name << "," << ap << "," << ar << "," << af1;
   for (const double& prec : precisions) {
